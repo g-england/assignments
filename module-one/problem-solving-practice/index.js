59377efb7b1b8b57db2a1b10ca9685b862e74ab4
Garrett England <garrett.t.england@gmail.com>
2020-11-19 20:57:34 -0700

    Assignment Problem Solving Practice added

--- module-one/problem-solving-practice/index.js
+++ module-one/problem-solving-practice/index.js
@@ -11,4 +11,30 @@ largestNumber = (array) => {
 
 largestNumber([-11, -4, -18, -20])
 
-//2. 
+//2. Special Characters
+function characters (array, character) {
+    var characterArr = []
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            if (array[i][j] === character) {
+                characterArr.push(array[i])
+            }
+        }
+    }
+    console.log(characterArr)
+}
+
+characters(["#3", "$$$", "C%4!", "Hey!"], "!")
+
+//3. Is Divisible Function
+
+function isDivisible(num1, num2) {
+    if (num1 % num2 === 0) {
+        console.log(true)
+    } else if (num1 % num2 != 0) {
+        console.log(false)
+    }
+}
+
+isDivisible(15, 4)
+
