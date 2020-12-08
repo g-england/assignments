c42ee65a90116d0283a01cd0ec4f4ec0d2529eb0
Garrett England <garrett.t.england@gmail.com>
2020-12-08 14:02:44 -0700

    Catch Me If You Can assignment added

---
+++ level-two/catch-me-if-you-can/index.js
@@ -0,0 +1,71 @@
+//1a) Write a function that returns the sum of two numbers. 
+//    Throw an error if either argument is not of the data type number:
+function sum(x, y){
+    try {
+        if (typeof(x) !== "number" || typeof(y) !== "number") {
+            throw new Error("Both parameters must be numbers.")
+        }
+        console.log("Both paramerters are numbers.")
+    }
+    catch(err){
+        console.log(err)
+    }
+    finally {
+        
+    }
+    return x + y;
+  }
+  
+  //1b)  Call the sum function inside a try block using "1" and "2" 
+  //     as arguments. Use console.log within a catch block to inform 
+  //     the user of the error.
+  try {
+    var s = sum(1, 2)
+  }
+  catch(err){
+      console.log("Both paramters must be numbers.")
+  }
+  finally {
+      console.log(s)
+      console.log("Test completed.")
+  }
+
+  //2a) Given a user object, write a function called login that takes 
+  //    a username and password as parameters. Throw an error if either 
+  //    of them don't match. Otherwise, log to the console a message 
+  //    saying "login successful!"
+  var user = {username: "sam", password: "123abc"};
+
+  function login(username, password){
+    try {
+        if (username !== user.username || password !== user.password){
+            throw new Error("Username or password incorrect.")
+        }
+        console.log("login successful!")
+    }
+    catch(err){
+        console.log(err)
+    }
+    finally{
+        console.log("Test complete")
+    }
+  }
+  
+
+  //2b) Call the login function within a try block. In one instance use 
+  //    the correct credentials, and in another use incorrect ones. Make sure 
+  //    you see the appropriate message!
+
+try {
+    login("sam", "123abc") 
+}
+catch(err){
+
+}
+finally {
+
+}
+
+
+
+
